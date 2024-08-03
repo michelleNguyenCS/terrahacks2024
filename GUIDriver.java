680c658b4396a299a7db48c9ee003c8a891b5933
michelle <168769494+michelleNguyenCS@users.noreply.github.com>
2024-08-03 08:32:42 -0400

    GUI has fields for Add and Clean tabs
    Home is still empty

--- GUIDriver.java
+++ GUIDriver.java
@@ -2,65 +2,160 @@
  * [CleanScape]
  * 
  * @author Michelle Nguyen
- * @version 2024-08-02
+ * @version 2024-08-03
  */
 
-// IMPORT STATEMENTS
+
+/* IMPORT STATEMENTS */
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 
 public class GUIDriver extends Application {
 	
 	@Override
 	public void start(Stage stage) throws Exception {
 		
-		// FRAME
-		VBox vbox = new VBox();
-		vbox.setAlignment(Pos.CENTER);
+		/* FRAME */
+		VBox vbox = new VBox();			// Page Frame
+		vbox.setAlignment(Pos.CENTER);	// Center in the middle
 		
-		// TITLE
-		//javafx.scene.text.Font.getFontNames();
+		/* FONTS */
 		Font titleFont = new Font("Quicksand", 56);
-		Text appTitle = new Text("CleanScape");
+		Font f1 = new Font("Quicksand", 24);
+		
+		/* APP TITLE */
+		Text appTitle = new Text("CleanScapes");
 		appTitle.setFont(titleFont);
 		vbox.getChildren().add(appTitle);
 		
-		// INPUT BOX FOR ADDRESS
-		HBox hbox = new HBox();
-		hbox.setAlignment(Pos.CENTER);
-		hbox.setSpacing(10);
+		/* TABS */
+		TabPane tabPane = new TabPane();
+		tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+		Tab homeTab = new Tab("Home");
+		Tab addTab = new Tab("Add");
+		Tab cleanTab = new Tab("Clean");
+		tabPane.getTabs().add(homeTab);
+		tabPane.getTabs().add(addTab);
+		tabPane.getTabs().add(cleanTab);
 		
-		TextField addressInput = new TextField();
-		addressInput.setPromptText("Enter Address");
-		addressInput.setPrefWidth(200);
-		HBox.setMargin(addressInput, new Insets(10, 10, 10, 10));	// (Top, Right, Bottom, Left)
+		// Frames
+		VBox homeVBox = new VBox(new Label("[ List Polluted Addresses ]"));
+		VBox addVBox = new VBox();
+		VBox cleanVBox = new VBox();
+		homeTab.setContent(homeVBox);
+		addTab.setContent(addVBox);
+		cleanTab.setContent(cleanVBox);
+		addVBox.setAlignment(Pos.CENTER);
+		cleanVBox.setAlignment(Pos.CENTER);
+		
+		/* F1 - HOME */
+		
+		// [Waiting on Kathryn's Code]
+		
+		/* F2 - ADD */
+		
+		Text addText = new Text("Add an Address");
+		addText.setFont(f1);
+		
+		// Input Box for Username
+		
+		HBox addUserHBox = new HBox();
+		addUserHBox.setAlignment(Pos.CENTER);
+		
+		Label addUserLabel = new Label("Enter Username:");
+		addUserLabel.setFont(f1);
+		
+		TextField addUserInput = new TextField();
+		addUserInput.setFont(f1);
+		addUserInput.setPromptText("Name..");
+		
+		addUserHBox.getChildren().addAll(addUserLabel, addUserInput);
+		
+		// Input Box for Address
+		
+		HBox addAddressHBox = new HBox();
+		addAddressHBox.setAlignment(Pos.CENTER);
+		
+		Label addAddressLabel = new Label("Enter Address:");
+		addAddressLabel.setFont(f1);
+		
+		TextField addAddressInput = new TextField();
+		addAddressInput.setFont(f1);
+		addAddressInput.setPromptText("Search..");
+		
+		addAddressHBox.getChildren().addAll(addAddressLabel, addAddressInput);
+		
+		// Submit Button
+		
+		Button addButton = new Button("Submit");
+		
+		addVBox.getChildren().addAll(addText, addUserHBox, addAddressHBox,addButton);
+		
+		/* F3 - CLEAN */
+		
+		Text cleanText = new Text("Submit a Cleaning");
+		cleanText.setFont(f1);
 
-		hbox.getChildren().add(addressInput);
-		vbox.getChildren().add(hbox);
+		// Input Box for Username
 		
-		Button searchButton = new Button("Search");
-		hbox.getChildren().add(searchButton);
+		HBox cleanUserHBox = new HBox();
+		cleanUserHBox.setAlignment(Pos.CENTER);
 		
+		Label cleanUserLabel = new Label("Enter Username:");
+		cleanUserLabel.setFont(f1);
 		
+		TextField cleanUserInput = new TextField();
+		cleanUserInput.setFont(f1);
+		cleanUserInput.setPromptText("Name..");
 		
-		// FINAL SETUP
-		Scene scene = new Scene(vbox, 600, 800);	// (Box, Width, Height)
-		stage.setScene(scene);						// Adds the scene to the stage
-		stage.setTitle("CleanScape");	// Title of Interface
+		cleanUserHBox.getChildren().addAll(cleanUserLabel, cleanUserInput);
+		
+		// Input Box for Address
+		
+		HBox cleanAddressHBox = new HBox();
+		cleanAddressHBox.setAlignment(Pos.CENTER);
+		
+		Label cleanAddressLabel = new Label("Enter Address:");
+		cleanAddressLabel.setFont(f1);
+		
+		TextField cleanAddressInput = new TextField();
+		cleanAddressInput.setFont(f1);
+		cleanAddressInput.setPromptText("Search..");
+		
+		cleanAddressHBox.getChildren().addAll(cleanAddressLabel, cleanAddressInput);
+		
+		// Submit Button
+		
+		Button cleanButton = new Button("Submit");
+		
+		cleanVBox.getChildren().addAll(cleanText, cleanUserHBox, cleanAddressHBox, cleanButton);
+		
+		/* ADD ALL TO SCENE */
+		vbox.getChildren().add(tabPane);
+		Scene scene = new Scene(vbox, 600, 800);
+		String css = this.getClass().getResource("style.css").toExternalForm();
+		scene.getStylesheets().add(css);
+		
+		/* FINAL GUI SETUP */
+		stage.setScene(scene);			// Adds the scene to the stage
+		stage.setTitle("CleanScapes");	// Title of Interface
 		stage.show();					// Required for Interface to Launch (1/2)
 	}
 
 	public static void main(String[] args) {
 		launch(args);	// Required for Interface to Launch (2/2)
 	}
-
 }
